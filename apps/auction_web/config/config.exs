0fb9c7b970e6d25a0d7f2b06d6f9673e64a99220
Zach Liss <zach.liss@dockyard.com>
2019-12-26 10:04:14 -0500

    bring in phoenix

---
+++ apps/auction_web/config/config.exs
@@ -0,0 +1,22 @@
+# Since configuration is shared in umbrella projects, this file
+# should only configure the :auction_web application itself
+# and only for organization purposes. All other config goes to
+# the umbrella root.
+use Mix.Config
+
+# General application configuration
+config :auction_web,
+  generators: [context_app: false]
+
+# Configures the endpoint
+config :auction_web, AuctionWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "2NM6Q1Up+3zauIznJq7EtEu+T2ubfR8oABvjtODn4Z+qt0fUFW1dLR1BhubI6eYR",
+  render_errors: [view: AuctionWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: AuctionWeb.PubSub, adapter: Phoenix.PubSub.PG2]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
+
+config :phoenix, :json_library, Jason
