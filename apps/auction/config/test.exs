ff8b507a8be8d83c237a7c73eddb74524d6cf695
Zach Liss <zach.liss@dockyard.com>
2020-01-10 10:56:00 -0500

    setup to test with Ecto

---
+++ apps/auction/config/test.exs
@@ -0,0 +1,12 @@
+# ran
+# > MIX_ENV=test mix do ecto.create, ecto.migrate
+# in order to setup the new auction_test db
+use Mix.Config
+
+config :auction, Auction.Repo,
+  database: "auction_test",
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  port: "5432",
+  pool: Ecto.Adapters.SQL.Sandbox
--- apps/auction/test/test_helper.exs
+++ apps/auction/test/test_helper.exs
@@ -1,1 +1,5 @@
+# this helper will be executed before every test run
+# you can put any setup code you need to run before your tests in this file
 ExUnit.start()
+
+Ecto.Adapters.SQL.Sandbox.mode(Auction.Repo, :manual)
