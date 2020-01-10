4e8a89eb0e93476c4286b78b1665017f7cb72e79
Zach Liss <zach.liss@dockyard.com>
2020-01-10 11:12:39 -0500

    first test: list_items/0

--- apps/auction/test/auction_test.exs
+++ apps/auction/test/auction_test.exs
@@ -1,6 +1,6 @@
 defmodule AuctionTest do
   use ExUnit.Case
-  alias Auction.{Repo}
+  alias Auction.{Item, Repo}
   doctest Auction
 
   setup do
@@ -9,5 +9,22 @@ defmodule AuctionTest do
 
   test "the truth" do
     assert true
+  end
+
+  describe "list_items/0" do
+    # ... tests
+    setup do
+      # insert items into db
+      {:ok, item1} = Repo.insert(%Item{title: "Item 1"})
+      {:ok, item2} = Repo.insert(%Item{title: "Item 2"})
+      {:ok, item3} = Repo.insert(%Item{title: "Item 3"})
+
+      # return a Map with a List of inserted items as the context
+      %{items: [item1, item2, item3]}
+    end
+
+    test "returns all Items in the database", %{items: items} do
+      assert items == Auction.list_items()
+    end
   end
 end
